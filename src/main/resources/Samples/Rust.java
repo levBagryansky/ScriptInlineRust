ab9ce2f9317450cba07795811f070d735cbee37c
levBagryansky <28lev11@gmail.com>
2023-02-03 21:02:29 +0300

    added jni call

--- src/main/resources/Samples/Rust.java
+++ src/main/resources/Samples/Rust.java
@@ -1,17 +1,27 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Rust {
+    private static Path libs = Paths.get("/home/tardis3/ScriptPrototype/src/main/resources/libs/");
     public static void main(String[] args) {
         fun1();
         fun2();
         fun3();
     }
-
-    static void fun1() {
-        System.out.println("fun1");
+    static void fun1(){
+        //Path lib = libs.normalize().resolve("lib1/target/debug/liblib1.so").toAbsolutePath();
+        Path lib = Paths.get("/home/tardis3/ScriptPrototype/src/main/resources/libs/lib1/target/debug/liblib1.so");
+        System.load(lib.toString());
+        System.out.println(lib1());
     }
+
     static void fun2() {
         System.out.println("fun2");
     }
+
     static void fun3() {
         System.out.println("fun3");
     }
+
+    private static native String lib1();
 }
